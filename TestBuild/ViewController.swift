db25d1682be138e9ed501867775195023b47cdef
Slav Sarafski <slav@spiritinvoker.com>
2023-07-12 13:04:48 +0300

    first commit

---
+++ TestBuild/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TestBuild
+//
+//  Created by Slav Sarafski on 12.07.23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
